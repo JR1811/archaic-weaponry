a156c68c6e4aa15fe4a4596a773c8e38852b59c0
JR1811 <janreichelt77@gmail.com>
2024-03-18 14:57:56 +0100

    initial commit

---
+++ src/main/java/net/shirojr/archaicweaponry/ArchaicWeaponryClient.java
@@ -0,0 +1,10 @@
+package net.shirojr.archaicweaponry;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArchaicWeaponryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/shirojr/archaicweaponry/ArchaicWeaponryDataGenerator.java
@@ -0,0 +1,11 @@
+package net.shirojr.archaicweaponry;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ArchaicWeaponryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
