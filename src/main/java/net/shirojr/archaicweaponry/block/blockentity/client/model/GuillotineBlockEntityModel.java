f44731e40e284c9ae192c107d5cecaab51b80cbf
JR1811 <janreichelt77@gmail.com>
2024-03-26 21:33:32 +0100

    fixed lighting issue for the block entity renderer

--- src/main/java/net/shirojr/archaicweaponry/block/blockentity/client/model/GuillotineBlockEntityModel.java
+++ src/main/java/net/shirojr/archaicweaponry/block/blockentity/client/model/GuillotineBlockEntityModel.java
@@ -8,39 +8,25 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.shirojr.archaicweaponry.registry.ArchaicWeaponryBlockEntityRenderers;
 
 public class GuillotineBlockEntityModel extends Model {
-
     private final ModelPart guillotine;
-    private final ModelPart plank, frame;
 
     public GuillotineBlockEntityModel(BlockEntityRendererFactory.Context context) {
         super(RenderLayer::getEntityCutout);
         ModelPart modelPart = context.getLayerModelPart(ArchaicWeaponryBlockEntityRenderers.GUILLOTINE_MODEL_LAYER);
         this.guillotine = modelPart.getChild("guillotine");
-        this.plank = this.guillotine.getChild("plank");
-        this.frame = this.guillotine.getChild("frame");
-
     }
 
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
-        ModelPartData guillotine = modelPartData.addChild("guillotine", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 24.0F, -8.0F));
-
-        ModelPartData plank = guillotine.addChild("plank", ModelPartBuilder.create().uv(4, 0).cuboid(-7.0F, -8.0F, 0.0F, 14.0F, 4.0F, 16.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
-
-        ModelPartData frame = guillotine.addChild("frame", ModelPartBuilder.create().uv(27, 20).cuboid(4.75F, -8.25F, 13.75F, 2.5F, 8.25F, 2.5F, new Dilation(0.0F))
-                .uv(27, 20).cuboid(-7.25F, -8.25F, 13.75F, 2.5F, 8.25F, 2.5F, new Dilation(0.0F))
-                .uv(48, 10).cuboid(-8.0F, -1.25F, 14.0F, 16.0F, 1.0F, 2.0F, new Dilation(0.0F))
-                .uv(27, 20).cuboid(-1.0F, -1.25F, 0.0F, 2.0F, 1.0F, 14.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData guillotine = modelPartData.addChild("guillotine", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 16.0F, 0.0F));
+        ModelPartData blade = guillotine.addChild("blade", ModelPartBuilder.create().uv(0, 9).cuboid(-5.0F, 1.0F, 0.0F, 10.0F, 7.0F, 0.0F, new Dilation(0.0F))
+                .uv(48, 13).cuboid(-4.0F, -0.25F, -0.5F, 8.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
         return TexturedModelData.of(modelData, 128, 128);
     }
 
     @Override
     public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha) {
-        // this.guillotine.render(matrices, vertices, light, overlay);
-        // this.plank.render(matrices, vertices, light, overlay);
-        // this.frame.render(matrices, vertices, light, overlay);
+        this.guillotine.render(matrices, vertices, light, overlay);
     }
-
-
 }
--- src/main/java/net/shirojr/archaicweaponry/block/blockentity/client/renderer/GuillotineBlockEntityRenderer.java
+++ src/main/java/net/shirojr/archaicweaponry/block/blockentity/client/renderer/GuillotineBlockEntityRenderer.java
@@ -11,6 +11,7 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RotationAxis;
 import net.minecraft.world.LightType;
 import net.shirojr.archaicweaponry.ArchaicWeaponry;
@@ -18,6 +19,7 @@ import net.shirojr.archaicweaponry.block.GuillotineBlock;
 import net.shirojr.archaicweaponry.block.blockentity.GuillotineBlockEntity;
 import net.shirojr.archaicweaponry.block.blockentity.client.model.GuillotineBlockEntityModel;
 import net.shirojr.archaicweaponry.util.helper.MultiBlockUtil;
+import org.lwjgl.system.MathUtil;
 
 public class GuillotineBlockEntityRenderer implements BlockEntityRenderer<GuillotineBlockEntity> {
     private static final Identifier GUILLOTINE_TEXTURE = new Identifier(ArchaicWeaponry.MODID, "textures/block/guillotine.png");
@@ -32,22 +34,19 @@ public class GuillotineBlockEntityRenderer implements BlockEntityRenderer<Guillo
         if (!(entity.getWorld() instanceof ClientWorld clientWorld)) return;
         BlockState state = entity.getCachedState();
         if (!state.contains(GuillotineBlock.PART) || !state.contains(GuillotineBlock.DIRECTION)) return;
-        if (!state.get(GuillotineBlock.PART).equals(MultiBlockUtil.GuillotinePart.TOP)) return;
+        if (!state.get(GuillotineBlock.PART).isOrigin()) return;
 
-        //TODO: only take in blade for rendering inside the custom model class
         Direction direction = state.get(GuillotineBlock.DIRECTION);
         matrices.push();
+        matrices.translate(0.5, 0, 0.5);
         matrices.multiply(direction.getOpposite().getRotationQuaternion());
         matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(90));
-        matrices.translate(0.5, -1, -0.5);
-
+        matrices.translate(0, -3.7, -0.45);
+        matrices.scale(0.95f, 0.95f, 0.95f);
         var layer = this.guillotineModel.getLayer(GUILLOTINE_TEXTURE);
         var vertexConsumer = vertexConsumers.getBuffer(layer);
-
-        guillotineModel.render(matrices, vertexConsumer,
-                LightmapTextureManager.getBlockLightCoordinates(clientWorld.getLightLevel(entity.getPos().up(2))),
-                overlay, 1, 1, 1, 1);
+        int lightLevel = WorldRenderer.getLightmapCoordinates(clientWorld, entity.getPos().offset(Direction.UP, 2));
+        guillotineModel.render(matrices, vertexConsumer, lightLevel, overlay, 1, 1, 1, 1);
         matrices.pop();
-
     }
 }
